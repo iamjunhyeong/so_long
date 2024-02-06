568daa088858e36acc66fe14a3a462ba8d067e28
iamjunhyeong <dddodo447@gmail.com>
2024-02-06 21:47:02 +0900

    feat keypress

--- hook.c
+++ hook.c
@@ -6,11 +6,11 @@
 /*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 17:43:00 by junhyeop          #+#    #+#             */
-/*   Updated: 2024/02/06 18:30:29 by junhyeop         ###   ########.fr       */
+/*   Updated: 2024/02/06 21:22:51 by junhyeop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
+#include "./mlx/mlx.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -23,16 +23,61 @@
 # define KEY_S			1
 # define KEY_D			2
 
-int	key_press(int keycode, t_data *data)
+// mlx 구조체
+typedef struct	s_vars {
+	void		*mlx;
+	void		*win;
+}				t_vars;
 
+// image data 구조체
+typedef struct s_data
+{
+	void 	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
+
+typedef struct s_param
+{
+	int	x;
+	int	y;
+}	t_param;
+
+void	param_init(t_param *param)
+{
+	param->x = 3;
+	param->y = 4;
+}
+
+int	key_press(int keycode, t_param *param)
+{
+	static int	a;
+	a = 0;
+
+	if (keycode == KEY_W)
+		param->y++;
+	else if (keycode == KEY_S)
+		param->y--;
+	else if (keycode == KEY_A)
+		param->x--;
+	else if (keycode == KEY_D)
+		param->x++;
+	else if (keycode == KEY_ESC)
+		exit (0);
+	printf("x: %d, y: %d\n", param->x, param->y); 
+	return (0);
+}
 
 int main(void)
 {
 	t_vars	vars;
+	t_param	param;
 
+	param_init(&param);
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
-	mlx_key_hook(vars.win, key_hook, &vars);
-	mlx_mouse_hook(vars.win, mouse_hook, &vars);
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "mlx_pj");
+	mlx_hook(vars.win, X_EVENT_KEY_RELEASE, 0, &key_press, &param);
 	mlx_loop(vars.mlx);
 }
