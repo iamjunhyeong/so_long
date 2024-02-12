81c03b2d78b229b1d98471e006d2cb2bb9e3cf8d
iamjunhyeong <dddodo447@gmail.com>
2024-02-12 21:56:31 +0900

    add lib

---
+++ libft/ft_printf/printf_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junhyeop <junhyeop@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 21:36:15 by junhyeop          #+#    #+#             */
+/*   Updated: 2023/11/13 17:24:36 by junhyeop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putstr(char *s)
+{
+	if (!s)
+		return (0);
+	while (*s)
+	{
+		if (write(1, s, 1) == -1)
+			return (0);
+		s++;
+	}
+	return (1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	n;
+
+	n = 0;
+	while (s[n] != 0)
+		n++;
+	return (n);
+}
